24ecd0679b0e23202d127339f2c211e2237283a7
vishal sable <sablevishal495@gmail.com>
2026-03-16 22:45:24 +0530

    Update check_env.py

--- air-canvas-main/air-canvas-main/check_env.py
+++ air-canvas-main/air-canvas-main/check_env.py
@@ -6,7 +6,7 @@ MAX_PY = (3, 11)
     
 def check_python_version(): 
     major, minor = sys.version_info[:2]
-    if (major, minor) < MIN_PY or (major, minor) > MAX_PY:
+    if (major, minor) < MIN_PY or (major, minor) > MAX_PY: 
         print(f"Unsupported Python version {major}.{minor}.{sys.version_info[2]}.")
         print("Mediapipe currently supports Python 3.8 - 3.11. Please install a supported Python and create a virtual environment.")
         return False
