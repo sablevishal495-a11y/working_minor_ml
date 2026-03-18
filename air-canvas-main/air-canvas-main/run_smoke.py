64ba09e5207e5fec4e89cac4a23356d42a0ca1a7
vishal sable <sablevishal495@gmail.com>
2026-03-18 22:28:18 +0530

    Update run_smoke.py

--- air-canvas-main/air-canvas-main/run_smoke.py
+++ air-canvas-main/air-canvas-main/run_smoke.py
@@ -7,7 +7,7 @@ print('numpy', np.__version__)
 print('mediapipe', mp.__version__)
 
 # Try to create a hands detector
-mp_hands = mp.solutions.hands  
+mp_hands = mp.solutions.hands 
 hands = mp_hands.Hands(max_num_hands=1) 
 print('Hands created')
   
